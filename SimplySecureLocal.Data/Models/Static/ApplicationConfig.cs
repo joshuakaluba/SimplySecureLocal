9e1d35ff4a402d43eb1191c3891abfa90107e1c8
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-03 22:49:59 -0500

    Removed database connection properties as moved to local SQLite database

--- SimplySecureLocal.Data/Models/Static/ApplicationConfig.cs
+++ SimplySecureLocal.Data/Models/Static/ApplicationConfig.cs
@@ -8,22 +8,6 @@ namespace SimplySecureLocal.Data.Models.Static
             = Environment.GetEnvironmentVariable
                 ("SIMPLY_SECURE_LOCAL_APPLICATION_PORT", target: EnvironmentVariableTarget.Process);
 
-        internal static string DatabaseName
-            = Environment.GetEnvironmentVariable
-                ("SIMPLY_SECURE_LOCAL_DB_NAME", target: EnvironmentVariableTarget.Process);
-
-        internal static string DatabaseUser
-             = Environment.GetEnvironmentVariable
-                ("SIMPLY_SECURE_LOCAL_DB_USER", target: EnvironmentVariableTarget.Process);
-
-        internal static string DatabasePassword
-            = Environment.GetEnvironmentVariable
-                ("SIMPLY_SECURE_LOCAL_DB_PASSWORD", target: EnvironmentVariableTarget.Process);
-
-        internal static string DatabaseHost
-            = Environment.GetEnvironmentVariable
-                ("SIMPLY_SECURE_LOCAL_DB_HOST", target: EnvironmentVariableTarget.Process);
-
         public static string BackendHost 
             = Environment.GetEnvironmentVariable
                 ("SIMPLY_SECURE_LOCAL_BACKEND_HOST", target: EnvironmentVariableTarget.Process);
