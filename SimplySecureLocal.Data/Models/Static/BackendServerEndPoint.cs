17684b0dac7bca99cab5aeca018fb8cb0ff44e66
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-03 22:50:30 -0500

    Created class to handle endpoints from the API

---
+++ SimplySecureLocal.Data/Models/Static/BackendServerEndPoint.cs
@@ -0,0 +1,9 @@
+﻿namespace SimplySecureLocal.Data.Models.Static
+{
+    internal static class BackendServerEndPoint
+    {
+        internal static string ModuleBoot => "/api/boot";
+
+        internal static string StateChange => "/api/statechange";
+    }
+}
