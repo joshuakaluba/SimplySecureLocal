7c08d98aa207f873bbdb31477b18ca51351dc54a
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-05 18:58:50 -0500

    Created Module class to keep track of unique modules

---
+++ SimplySecureLocal.Data/Models/Module.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace SimplySecureLocal.Data.Models
+{
+    public sealed class Module : Auditable
+    {
+        public Module()
+        {
+        }
+
+        public Module(Guid id, bool state)
+        {
+            Id = id;
+            State = state;
+        }
+
+        public DateTime LastHeartBeat { get; set; } = DateTime.Now;
+
+        [JsonProperty("state")]
+        public bool State { get; set; }
+    }
+}
