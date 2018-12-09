6bff089f87bc5a038a999c1fc5d03e1a0e6393d8
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-09 11:06:17 -0500

    Added new migration, renamed StateChange entity to 'ModuleEvent'.

---
+++ SimplySecureLocal.Data/Migrations/20181209155633_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimplySecureLocal.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BootMessages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    ModuleId = table.Column<Guid>(nullable: false),
+                    State = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BootMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Heartbeats",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    ModuleId = table.Column<Guid>(nullable: false),
+                    State = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heartbeats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ModuleEvents",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    ModuleId = table.Column<Guid>(nullable: false),
+                    State = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModuleEvents", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Modules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    LastHeartbeat = table.Column<DateTime>(nullable: false),
+                    State = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Modules", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BootMessages");
+
+            migrationBuilder.DropTable(
+                name: "Heartbeats");
+
+            migrationBuilder.DropTable(
+                name: "ModuleEvents");
+
+            migrationBuilder.DropTable(
+                name: "Modules");
+        }
+    }
+}
