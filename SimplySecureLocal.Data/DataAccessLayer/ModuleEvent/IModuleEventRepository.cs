db79aa9ddb65180fb65da5ee00a441bf103269c8
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-13 08:21:04 -0500

    Created interface for Module Events

---
+++ SimplySecureLocal.Data/DataAccessLayer/ModuleEvent/IModuleEventRepository.cs
@@ -0,0 +1,15 @@
+﻿using SimplySecureLocal.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimplySecureLocal.Data.DataAccessLayer.ModuleEvent
+{
+    public interface IModuleEventRepository
+    {
+        Task CreateModuleEvent(Models.ModuleEvent moduleEvent);
+
+        Task<List<Models.ModuleEvent>> GetModuleEvents();
+
+        Task<ModuleResponse> PostModuleEvent(Models.ModuleEvent moduleEvent);
+    }
+}
