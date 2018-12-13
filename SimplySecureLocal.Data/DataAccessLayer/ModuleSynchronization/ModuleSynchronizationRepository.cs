93199fa8d3414424db7fecf6c6900d067f146981
Joshua Kaluba <joshkaluba@gmail.com>
2018-12-13 08:22:49 -0500

    Created implementation for Module sync

---
+++ SimplySecureLocal.Data/DataAccessLayer/ModuleSynchronization/ModuleSynchronizationRepository.cs
@@ -0,0 +1,33 @@
+﻿using SimplySecureLocal.Common.Exception;
+using SimplySecureLocal.Data.DataAccessLayer.Module;
+using SimplySecureLocal.Data.Models.Static;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SimplySecureLocal.Data.DataAccessLayer.ModuleSynchronization
+{
+    public class ModuleSynchronizationRepository : BaseRepository, IModuleSynchronizationRepository
+    {
+        public async Task SynchronizeModules(IModuleRepository moduleRepository)
+        {
+            var modules = await moduleRepository.GetModules();
+
+            if (modules != null && modules.Count > 0)
+            {
+                using (var client = ApplicationHttpClient(ApplicationConfig.BackendUri))
+                {
+                    var response
+                        = await client.PostAsJsonAsync
+                            (BackendServerEndPoint.SynchronizeModules, modules);
+
+                    if (response.IsSuccessStatusCode == false)
+                    {
+                        var ex = ApiException.CreateApiException(response);
+
+                        throw ex;
+                    }
+                }
+            }
+        }
+    }
+}
